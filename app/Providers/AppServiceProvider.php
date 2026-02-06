76c3b2ab45c2ca2f4a112de9a2a0cd6b0bd60f34
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-06 20:27:59 +0700

    [update] app service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\DataAsetKolektif;
+use App\Observers\DataAsetKolektifObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register Observer for DataAsetKolektif
+        DataAsetKolektif::observe(DataAsetKolektifObserver::class);
     }
 }
