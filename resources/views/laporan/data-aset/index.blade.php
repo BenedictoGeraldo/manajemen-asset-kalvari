c72ed00f2139cb8a6bf1feeca54197ec1114e55e
BenedictoGeraldo <geraldodawa@gmail.com>
2026-08-18 14:32:23 +0700

    fix: remove unnecessary comment and adjust indentation in laporan data aset view

--- resources/views/laporan/data-aset/index.blade.php
+++ resources/views/laporan/data-aset/index.blade.php
@@ -3,14 +3,12 @@
 @section('title', 'Laporan Data Aset')
 @section('page-title', 'Laporan Data Aset')
 
-// this is laporan data asset report page
-
 @section('content')
 <div class="p-6">
     @if(session('success'))
         <div class="mb-4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
             <span class="block sm:inline">{{ session('success') }}</span>
-        </div>
+    </div>
     @endif
 
     @if(session('error'))
